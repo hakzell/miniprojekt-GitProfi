31106546fb2379c29b36a090a156544bce59f9ca
Leon Reitenbach <lreitenbach@schueler.hakzell.at>
2025-05-06 08:53:51 +0200

    Projektdateien hinzufügen.

---
+++ smartwork/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace smartwork
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
