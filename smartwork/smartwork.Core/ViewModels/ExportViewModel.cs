b6116f0f5106a466280433561f2bb801ff5c6b4d
Leon Reitenbach <lreitenbach@schueler.hakzell.at>
2025-05-20 09:01:32 +0200

    ExportPage

---
+++ smartwork/smartwork.Core/ViewModels/ExportViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace smartwork.Core.ViewModels;
+
+public class ExportViewModel
+{
+}
---
+++ smartwork/smartwork/Pages/ExportPage.xaml.cs
@@ -0,0 +1,9 @@
+namespace smartwork.Pages;
+
+public partial class ExportPage : ContentPage
+{
+	public ExportPage()
+	{
+		InitializeComponent();
+	}
+}
