53cbeb791adda85c2becfd32e14161ca8c91844b
Leon Reitenbach <lreitenbach@schueler.hakzell.at>
2025-05-20 13:01:15 +0200

    Toolkit

--- smartwork/smartwork.Core/ViewModels/MainVIewModel.cs
+++ smartwork/smartwork.Core/ViewModels/MainVIewModel.cs
@@ -1,12 +1,16 @@
-﻿using System;
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Timers;
 
 namespace smartwork.Core.ViewModels;
 
-public class MainViewModel
+public partial class MainViewModel : ObservableObject
 {
+
 
 }
