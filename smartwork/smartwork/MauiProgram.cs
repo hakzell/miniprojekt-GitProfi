6169e7a10eea5e5cd4fde8e791114d1df780b261
rsteger <rsteger@schueler.hakzell.at>
2025-05-20 12:09:09 +0200

    ManualPage, ExportPage Konstrukt erstellt.

--- smartwork/smartwork/MauiProgram.cs
+++ smartwork/smartwork/MauiProgram.cs
@@ -19,6 +19,8 @@ namespace smartwork
 
             builder.Services.AddSingleton<ExportPage>();
             builder.Services.AddSingleton<ExportViewModel>();
+            builder.Services.AddSingleton<ManualPage>();
+            builder.Services.AddSingleton<ManualViewModel>();
 
 #if DEBUG
             builder.Logging.AddDebug();
