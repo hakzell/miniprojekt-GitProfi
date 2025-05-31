c886bb11de971bf8ff835db38d671a62e8cb5d93
Leon Reitenbach <LReitenbach@schueler.hakzell.at>
2025-05-31 22:50:07 +0200

    Manual ViewModel überarbeitet

---
+++ smartwork/smartwork.Core/Messages/ProjectSavedMessage.cs
@@ -0,0 +1,16 @@
+﻿using CommunityToolkit.Mvvm.Messaging.Messages;
+using smartwork.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace smartwork.Core.Messages;
+
+public class ProjectSavedMessage : ValueChangedMessage<Project>
+{
+    public ProjectSavedMessage(Project value) : base(value)
+    {
+    }
+}
--- smartwork/smartwork.Core/ViewModels/ManualViewModel.cs
+++ smartwork/smartwork.Core/ViewModels/ManualViewModel.cs
@@ -1,25 +1,39 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
-using System.Collections.ObjectModel;
+using CommunityToolkit.Mvvm.Messaging;
+using smartwork.Core.Messages;
 using smartwork.Data.Models;
+using smartwork.Data.Services;
+using System.Collections.ObjectModel;
 
 namespace smartwork.ViewModels;
 
 public partial class ManualViewModel : ObservableObject
 {
+    IRepository _repository;
+
+    public ManualViewModel(IRepository repository)
+    {
+        _repository = repository;
+    }
+
     [ObservableProperty]
+    [NotifyCanExecuteChangedFor(nameof(SaveCommand))] // Sobald was geändert wird, wird SaveCommand aufgerufen, um die Ausführbarkeit zu prüfen
     private DateTime selectedDate = DateTime.Today;
 
     [ObservableProperty]
+    [NotifyCanExecuteChangedFor(nameof(SaveCommand))]
     private TimeSpan fromTime = TimeSpan.Zero;
 
     [ObservableProperty]
+    [NotifyCanExecuteChangedFor(nameof(SaveCommand))]
     private TimeSpan toTime = TimeSpan.Zero;
 
     [ObservableProperty]
+    [NotifyCanExecuteChangedFor(nameof(SaveCommand))]
     private string description = string.Empty;
 
-    public ObservableCollection<Project> Entries { get; } = new();
+    public ObservableCollection<Project> projects { get; } = new(); 
 
     public ManualViewModel()
     {
@@ -28,7 +42,7 @@ public partial class ManualViewModel : ObservableObject
     }
 
     [RelayCommand]
-    private void SaveLocal()
+    void Save()
     {
         if (ToTime <= FromTime)
         {
@@ -36,12 +50,18 @@ public partial class ManualViewModel : ObservableObject
             return;
         }
 
-        var project = new Project(SelectedDate, FromTime, ToTime, description);
-        Entries.Add(project);
+        Project project = new Project(this.SelectedDate, this.FromTime, this.ToTime, this.Description);
 
-        // Eingabefelder zurücksetzen (optional)
-        SelectedDate = DateTime.Today;
-        FromTime = TimeSpan.Zero;
-        ToTime = TimeSpan.Zero;
+        var result = _repository.Save(project);
+        if (result)
+        {
+            WeakReferenceMessenger.Default.Send(new ProjectSavedMessage(project));
+
+            this.SelectedDate = DateTime.Today;
+            this.FromTime = new TimeSpan(8, 0, 0);
+            this.ToTime = new TimeSpan(17, 0, 0);
+            this.Description = string.Empty;
+
+        }
     }
 }
